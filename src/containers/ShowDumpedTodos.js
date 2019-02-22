a117ac3ed444467415aa5277def06c13cf5665ee
Sviatlana Viarbitskaya <sviatlana.viarbitskaya@gmail.com>
2019-02-22 13:34:57 +0100

    correct ShowDumpedTodos.js

--- src/containers/ShowDumpedTodos.js
+++ src/containers/ShowDumpedTodos.js
@@ -11,7 +11,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  toggleTodo: id => dispatch(toggleDump(id)),
+  toggleDump: id => dispatch(toggleDump(id)),
   deleteTodo: id => dispatch(deleteTodo(id))
 })
 
